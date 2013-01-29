1f11d5fa3f20cf21e569f016db13e444cfec5a8a
Ray Zhang <zhang_yimin007@live.cn>
2013-01-29 16:26:55 +0800

    Add Core Telephony Category And Delete Mobile Gestalt Categroy
    Change Mobile Gestalt Categroy to Mobile Gestalt Category

---
+++ UIDevice+CoreTelephonyCategory.h
@@ -0,0 +1,13 @@
+//
+//  UIDevice+CoreTelephonyCategory.h
+//  EquipmentInfo
+//
+//  Created by Ray Zhang on 13-1-29.
+//  Copyright (c) 2013年 Ray Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIDevice (CoreTelephony)
+
+@end
---
+++ UIDevice+CoreTelephonyCategory.m
@@ -0,0 +1,13 @@
+//
+//  UIDevice+CoreTelephonyCategory.m
+//  EquipmentInfo
+//
+//  Created by Ray Zhang on 13-1-29.
+//  Copyright (c) 2013年 Ray Zhang. All rights reserved.
+//
+
+#import "UIDevice+CoreTelephonyCategory.h"
+
+@implementation UIDevice (CoreTelephony)
+
+@end
