5cf70ee4baef6d87922eba3a0a85d7b5730db7ea
Ray Zhang <zhang_yimin007@live.cn>
2013-01-30 10:09:08 +0800

    update CoreTelephony Category

--- UIDevice+CoreTelephonyCategory.h
+++ UIDevice+CoreTelephonyCategory.h
@@ -5,9 +5,20 @@
 //  Created by Ray Zhang on 13-1-29.
 //  Copyright (c) 2013年 Ray Zhang. All rights reserved.
 //
+//  This Category Depand on Core Telephony Framework.
+//
 
 #import <UIKit/UIKit.h>
 
 @interface UIDevice (CoreTelephony)
+
+- (NSString *)IMEI;
+- (NSString *)CMID; // Current Mobile Identifier. Genernally, it's same as IMEI.
+- (NSString *)ICCID;
+
+- (NSString *)IMSI;
+- (NSString *)CSID; // Current Subscriber Identifier. Genernally, it is same as IMSI, but CDMA carrier is not.
+
+- (NSString *)MEID; // Just for CDMA carrier.
 
 @end
--- UIDevice+CoreTelephonyCategory.m
+++ UIDevice+CoreTelephonyCategory.m
@@ -5,9 +5,61 @@
 //  Created by Ray Zhang on 13-1-29.
 //  Copyright (c) 2013年 Ray Zhang. All rights reserved.
 //
+//  This Category Depand on Core Telephony Framework.
+//
 
 #import "UIDevice+CoreTelephonyCategory.h"
 
+typedef struct CTResult {
+    int flag;
+    int a;
+} CTResult;
+
+extern struct CTServerConnection *_CTServerConnectionCreate(CFAllocatorRef, int (*)(void *, CFStringRef, CFDictionaryRef, void *), int *);
+extern int *_CTServerConnectionCopyMobileEquipmentInfo(CTResult *status, struct CTServerConnection *connection, CFMutableDictionaryRef *equipmentInfo);
+extern CFTypeRef _CTServerConnectionCopyPreferredPhoneNumber(CTResult *status, struct CTServerConnection *connection, CFMutableDictionaryRef *equipmentInfo);
+static int callback(void *connection, CFStringRef string, CFDictionaryRef dictionary, void *data) {
+    return 0;
+}
+
 @implementation UIDevice (CoreTelephony)
+
+// Core Telephony Device Information
+
+- (NSString *)coreTelephonyInfoForKey:(const NSString *)key {
+    NSString *retVal = nil;
+    struct CTServerConnection *ctsc = _CTServerConnectionCreate(kCFAllocatorDefault, callback, NULL);
+    struct CTResult result;
+    CFMutableDictionaryRef equipmentInfo = nil;
+    _CTServerConnectionCopyMobileEquipmentInfo(&result, ctsc, &equipmentInfo);
+    if (equipmentInfo) {
+        retVal = CFDictionaryGetValue(equipmentInfo, key);
+    }
+    return retVal;
+}
+
+- (NSString *)IMEI {
+    return [self coreTelephonyInfoForKey:@"kCTMobileEquipmentInfoIMEI"];
+}
+
+- (NSString *)CMID {
+    return [self coreTelephonyInfoForKey:@"kCTMobileEquipmentInfoCurrentMobileId"];
+}
+
+- (NSString *)ICCID {
+    return [self coreTelephonyInfoForKey:@"kCTMobileEquipmentInfoICCID"];
+}
+
+- (NSString *)MEID {
+    return [self coreTelephonyInfoForKey:@"kCTMobileEquipmentInfoMEID"];
+}
+
+- (NSString *)IMSI {
+    return [self coreTelephonyInfoForKey:@"kCTMobileEquipmentInfoIMSI"];
+}
+
+- (NSString *)CSID {
+    return [self coreTelephonyInfoForKey:@"kCTMobileEquipmentInfoCurrentSubscriberId"];
+}
 
 @end
